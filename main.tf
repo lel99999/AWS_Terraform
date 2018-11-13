bb52a60a55613e4424bd2cd0393b9ee699d06b95
lel99999 <luan.le@cfpb.gov>
2018-11-13 18:37:48 -0500

    upd working terraform code

--- main.tf
+++ main.tf
@@ -1,63 +1,19 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_instance" "dev01" {
-  ami = "ami-6057e21a"
+  ami = "ami-6871a115"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
-  key_name = "test-keypair"
+#  vpc_security_group_ids = ["vpc-28675b41"]
+  subnet_id = "subnet-5105e318"
+# vpc_security_group_ids = ["${aws_security_group.dev01.id}"]
+# subnet_id = "${aws_subnet.us-east-1a-private.id}"
+#security_groups = [
+#  "${aws_security_group.allow_ssh.name}", 
+#  "${aws_seucrity_group.allow_outbound.name}"
+#]
+# output "public_ip" {
+#   value = "${aws_instance.dev01.public_ip}"
+# }
+
   tags {
-    Name = "terraform-dev01"
+    name = "dev01"
   }
-}
-
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
-
-  ingress {
-    from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-output "public_ip" {
-  value = "${aws_instance.dev01.public_ip}"
 }
